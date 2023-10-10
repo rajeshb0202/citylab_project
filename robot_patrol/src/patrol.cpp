ba97c607ee2d8b8fe33bf200d3cf907bf702b319
rajeshb0202 <rajeshb0202@gmail.com>
2023-10-10 05:18:03 +0000

    added a method to find the angle for the maximum laser scan distance.

--- robot_patrol/src/patrol.cpp
+++ robot_patrol/src/patrol.cpp
@@ -3,6 +3,7 @@
 #include "rclcpp/logging.hpp"
 #include "rclcpp/utilities.hpp"
 #include "sensor_msgs/msg/detail/laser_scan__struct.hpp"
+#include <cmath>
 #include <rclcpp/rclcpp.hpp>
 #include <geometry_msgs/msg/twist.hpp>
 #include <sensor_msgs/msg/laser_scan.hpp>
@@ -24,12 +25,43 @@ class Patrol: public rclcpp::Node
     private:
         void laser_scan_callback(const sensor_msgs::msg::LaserScan::SharedPtr msg)
         {
-            RCLCPP_INFO(this->get_logger(), "front laser distance: %f", msg->ranges[180]);
+            laser_scan_msg = msg;
+            //RCLCPP_INFO(this->get_logger(), "angle range of the laser %d", laser_scan_msg->ranges.size());
+            //RCLCPP_INFO(this->get_logger(), "min ngle: %f, max angle: %f, increment: %f", msg->angle_min, msg->angle_max, msg->angle_increment);
+            direction_ = this->get_angle();
+            RCLCPP_INFO(this->get_logger(), "direction: %f", direction_);
+
         }
 
 
-        rclcpp::Subscription<sensor_msgs::msg::LaserScan>::SharedPtr laser_scan_subscriber;
 
+        //method to calculate maximum angle
+        float get_angle()
+        {
+            float max_angle = laser_scan_msg->ranges[0];
+            int max_angle_index = 0;
+            float angle;
+
+            //finding the index for the maximum distance
+            for (int i= 0; i< 360; i++)
+            {
+                if (!std::isinf(laser_scan_msg->ranges[i]) && max_angle < (laser_scan_msg->ranges[i]) )
+                {
+                    max_angle = laser_scan_msg->ranges[i];
+                    max_angle_index = i;
+                }
+            }
+            
+            angle =  (max_angle_index * (laser_scan_msg->angle_increment)) - (M_PI_2);
+
+            return angle;
+        }
+
+
+        //member variables
+        rclcpp::Subscription<sensor_msgs::msg::LaserScan>::SharedPtr laser_scan_subscriber;
+        float direction_;
+        sensor_msgs::msg::LaserScan::SharedPtr laser_scan_msg;
 };
 
 
