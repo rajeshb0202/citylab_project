36196a5d75003c70bf4ca7a6e3d9776a48842297
rajeshb0202 <rajeshb0202@gmail.com>
2023-10-11 03:34:28 +0000

    added callback groups and multi-threaded executors.

--- robot_patrol/src/patrol.cpp
+++ robot_patrol/src/patrol.cpp
@@ -1,6 +1,8 @@
 #include "geometry_msgs/msg/detail/twist__struct.hpp"
+#include "rclcpp/callback_group.hpp"
 #include "rclcpp/create_subscription.hpp"
 #include "rclcpp/executors.hpp"
+#include "rclcpp/executors/multi_threaded_executor.hpp"
 #include "rclcpp/logging.hpp"
 #include "rclcpp/publisher.hpp"
 #include "rclcpp/timer.hpp"
@@ -20,9 +22,16 @@ class Patrol: public rclcpp::Node
     public:
         Patrol(): Node("robot_patrol")
         {
-            laser_scan_subscriber = this->create_subscription<sensor_msgs::msg::LaserScan>("/scan", 10, std::bind(&Patrol::laser_scan_callback, this, std::placeholders::_1));
+            callback_group_pub = this->create_callback_group(rclcpp::CallbackGroupType::MutuallyExclusive);
+            //callback_group_sub = this->create_callback_group(rclcpp::CallbackGroupType::MutuallyExclusive);
+            
+            auto sub_options = rclcpp::SubscriptionOptions();
+            sub_options.callback_group = callback_group_pub;
+
+
+            laser_scan_subscriber = this->create_subscription<sensor_msgs::msg::LaserScan>("/scan", 1, std::bind(&Patrol::laser_scan_callback, this, std::placeholders::_1), sub_options);
             vel_publisher = this->create_publisher<geometry_msgs::msg::Twist>("/cmd_vel", 1);
-            callback_timer = this->create_wall_timer(500ms, std::bind(&Patrol::publisher_callback, this));
+            callback_timer = this->create_wall_timer(5ms, std::bind(&Patrol::publisher_callback, this), callback_group_pub);
 
             RCLCPP_INFO(this->get_logger(), "robot patrol node started...");
         }
@@ -34,29 +43,33 @@ class Patrol: public rclcpp::Node
         {
             laser_scan_msg = msg;
             direction_ = this->get_angle();
-            RCLCPP_INFO(this->get_logger(), "direction: %f", direction_);
+           // RCLCPP_INFO(this->get_logger(), "direction: %f", direction_);
         }
 
 
         //method to calculate angle for maximum distance
         float get_angle()
         {
-            float max_angle = laser_scan_msg->ranges[0];
-            int max_angle_index = 0;
-            float angle;
+           
+            float max_distance = laser_scan_msg->ranges[0];  // start with a negative value to ensure any valid distance will be greater
+            int max_distance_index = 0;
+            float angle = 0;
 
-            //finding the index for the maximum distance
-            for (int i= 0; i< 360; i++)
+            // Iterate over the ranges readings
+            for (int i= 0; i < 360; i++)
             {
-                if (!std::isinf(laser_scan_msg->ranges[i]) && max_angle < (laser_scan_msg->ranges[i]) )
+                if (!std::isinf(laser_scan_msg->ranges[i]) && max_distance < laser_scan_msg->ranges[i])
                 {
-                    max_angle = laser_scan_msg->ranges[i];
-                    max_angle_index = i;
+                    max_distance = laser_scan_msg->ranges[i];
+                    max_distance_index = i;
                 }
             }
             
-            angle =  (max_angle_index * (laser_scan_msg->angle_increment)) - (M_PI_2);
-
+            angle =  0.25* (max_distance_index * laser_scan_msg->angle_increment);
+            RCLCPP_INFO(this->get_logger(), "at 0 degrees: %f", laser_scan_msg->ranges[0]);
+            RCLCPP_INFO(this->get_logger(), "at 90 degrees: %f", laser_scan_msg->ranges[179]);
+            RCLCPP_INFO(this->get_logger(), "at 180 degrees: %f", laser_scan_msg->ranges[359]);
+            RCLCPP_INFO(this->get_logger(), "at 270 degrees: %f", laser_scan_msg->ranges[539]);
             return angle;
         }
 
@@ -67,7 +80,7 @@ class Patrol: public rclcpp::Node
             vel_data.linear.x = 0.1;
             vel_data.angular.z = direction_/2;
             vel_publisher->publish(vel_data);
-            RCLCPP_INFO(this->get_logger(), "inside publisher callback method");
+            //RCLCPP_INFO(this->get_logger(), "inside publisher callback method");
         }
 
 
@@ -75,6 +88,9 @@ class Patrol: public rclcpp::Node
         rclcpp::Subscription<sensor_msgs::msg::LaserScan>::SharedPtr laser_scan_subscriber;
         rclcpp::Publisher<geometry_msgs::msg::Twist>::SharedPtr vel_publisher;
         rclcpp::TimerBase::SharedPtr callback_timer;
+        rclcpp::CallbackGroup::SharedPtr callback_group_sub;
+        rclcpp::CallbackGroup::SharedPtr callback_group_pub;
+
         float direction_;
         sensor_msgs::msg::LaserScan::SharedPtr laser_scan_msg;
         geometry_msgs::msg::Twist vel_data;
@@ -88,8 +104,12 @@ int main(int argc, char** argv)
 {
     rclcpp::init(argc, argv);
     auto node = std::make_shared<Patrol>();
-    
-    rclcpp::spin(node);
+
+    //creating multiple threaded executor
+    rclcpp::executors::MultiThreadedExecutor executor_;
+    executor_.add_node(node);
+    executor_.spin();
+    //rclcpp::spin(node);
     rclcpp::shutdown();
     return 0;  
 }
