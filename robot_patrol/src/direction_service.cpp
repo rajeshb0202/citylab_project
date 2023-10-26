5223910afae76e4dadb368c0b685b4a4be1e832f
rajeshb0202 <rajeshb0202@gmail.com>
2023-10-26 06:40:19 +0000

    Further improvement of the condition - obstacle avoidance. the robot will go straight until if finds any obstacle in the front. if there is any obstacle present, then it will change the direction.

--- robot_patrol/src/direction_service.cpp
+++ robot_patrol/src/direction_service.cpp
@@ -26,57 +26,71 @@ class DirectionService: public rclcpp::Node
         void compute_direction_callback(const std::shared_ptr<CustomServiceMessage::Request> request, const std::shared_ptr<CustomServiceMessage::Response> response)
         {
             float total_dist_sec_left = 0, total_dist_sec_front = 0, total_dist_sec_right = 0;
-            float gap_threshold= 0.4;
+            float gap_threshold= 0.6;
+            bool chances_obstacles_present = false;
+            int front_range = 120;
 
-            //Computing right sum
-            for (int i = 180; i<300; i++)
+
+            //The robot will go straight unless there is a obstacle present on the front. 
+            //If there is any obstacle present in the front, then it will change the direction.
+            for (int i = 360 - front_range; i<=360+front_range; i= i+2)
             {
-                total_dist_sec_right += request->laser_data.ranges[i];
-
-                /*adding a condition to check obstacle. if there is any obstacle, then thata section 
-                would be skipped.The skipping condition is done by zeroing the the total sum of that section.*/
-                if(request->laser_data.ranges[i]<gap_threshold)
+                if (request->laser_data.ranges[i]< gap_threshold)
                 {
-                    total_dist_sec_right = 0;
-                    break;
+                    chances_obstacles_present = true;
                 }
             }
 
-            //Computing front sum
-            for (int i = 300; i<420; i++)
+            //if there is any obstacle present, then it will compute the direction.
+            if (chances_obstacles_present)
             {
-                total_dist_sec_front += request->laser_data.ranges[i];
-                if(request->laser_data.ranges[i]<gap_threshold)
+                //Computing right sum
+                for (int i = 180; i<300; i++)
                 {
-                    total_dist_sec_front = 0;
-                    break;
+                    total_dist_sec_right += request->laser_data.ranges[i];
+                    if (request->laser_data.ranges[i] < gap_threshold)
+                    {
+                        total_dist_sec_right = 0;
+                    }
                 }
-            }
 
-            //Computing left sum
-            for (int i = 420; i<540; i++)
-            {
-                total_dist_sec_left += request->laser_data.ranges[i];
-                if(request->laser_data.ranges[i]<gap_threshold)
+                //Computing front sum
+                for (int i = 300; i<420; i++)
                 {
-                    total_dist_sec_left = 0;
-                    break;
+                    total_dist_sec_front += request->laser_data.ranges[i];
+                    if (request->laser_data.ranges[i] < gap_threshold)
+                    {
+                        total_dist_sec_front = 0;
+                    }
                 }
-            }
 
 
-            //Deciding the response i.e. direction based upon the higher sum
-            if (total_dist_sec_right >= total_dist_sec_front && total_dist_sec_right >= total_dist_sec_left)
-            {
-                response->direction = "right";
-            }
-            else if (total_dist_sec_front >= total_dist_sec_right && total_dist_sec_front >= total_dist_sec_left)
-            {
-                response->direction = "front";
-            }
-            else
-            {
-                response->direction = "left";
+                //Computing left sum
+                for (int i = 420; i<540; i++)
+                {
+                    total_dist_sec_left += request->laser_data.ranges[i];
+                    if (request->laser_data.ranges[i] < gap_threshold)
+                    {
+                        total_dist_sec_left = 0;
+                    }
+                }
+                
+
+                //Deciding the response i.e. direction based upon the higher sum
+                if (total_dist_sec_right >= total_dist_sec_front && total_dist_sec_right >= total_dist_sec_left)
+                {
+                    response->direction = "right";
+                }
+                else if (total_dist_sec_front >= total_dist_sec_right && total_dist_sec_front >= total_dist_sec_left)
+                {
+                    response->direction = "front";
+                }
+                else
+                {
+                    response->direction = "left";
+                }
+            } else {
+            response->direction = "front";
             }
 
         }
